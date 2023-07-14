2edf07b11eb2acf05aebd345e340487090d6e8e4
LeiaWebDev <leia@cowmeuh.net>
2023-07-14 10:05:48 +0200

    lab memory game done

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ const cards = [
 
 const memoryGame = new MemoryGame(cards);
 
-window.addEventListener('load', (event) => {
+window.addEventListener('load',(event) => {
   let html = '';
   memoryGame.cards.forEach((pic) => {
     html += `
@@ -44,8 +44,125 @@ window.addEventListener('load', (event) => {
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
-    });
+
+      // option 1 :
+      html -= `
+      <div class="card" data-card-name="${pic.name}">
+        <div class="back" name="${pic.img}"></div>
+        <div class="front" style="background: url(img/${pic.img}) no-repeat"></div>
+      </div>
+    `;
+      html += `
+      <div class="card turned" data-card-name="${pic.name}">
+        <div class="back" name="${pic.img}"></div>
+        <div class="front" style="background: url(img/${pic.img}) no-repeat"></div>
+      </div>
+    `;
+
+    //OPTION 2:
+    // card.classList.toggle("turned")
+    //DOM.button.addEventListener( 'click', toggleUl, DOM.button.addEventListener( 'click', toggleUl, fafalse );
+    //DOM.ul.addEventListener( 'click', selectLi, false );
+  //}
+    console.log(`Card clicked: ${card}`);
+   
   });
+
+  
+  
+//let cards = 
 });
+// document.querySelectorAll('.memory-board').forEach((card) => {
+//   card.addEventListener('click', () => {;
+// memoryGame.cards.forEach((card) => {
+  let firstCard;
+  let secondCard;
+  let matchCards = 0;  
+memoryGame.cards.forEach((card) => {
+  card.addEventListener('click', () => {
+      if (!firstCard && !secondCard) {
+        firstCard = card;
+        card.classList.add('tuned');
+      } else if (firstCard && !secondCard) {
+        secondCard = card;
+        card.classList.add('turned');
+
+        if (firstCard.innerHTML === secondCard.innerHTML) {
+          firstCard = null;
+          secondCard = null;
+          document.querySelector('.player-message').innerHTML = 'Match';
+          document.querySelector('.player-message').style.color = '#55aa00';
+          matchCounter++;
+          if (matchCounter >= 12) {
+            document.querySelector('.player-message').innerHTML = 'All cards matched';
+            document.querySelector('.player-message').style.color = '#55aa00';
+          } else {
+            first.classList.remove('tuned');
+            second.classList.remove('tuned');
+            document.querySelector('.player-message').innerHTML = 'No Match';
+            document.querySelector('.player-message').style.color = '#e4000f';
+            setTimeout(() => {
+              firstCard.classList.remove('tuned');
+              secondCard.classList.remove('tuned');
+              firstCard = null;
+              secondCard = null
+            }, 2000);
+          }
+        }
+      }
+    })
+  });
+  matchCards();
+})
+
+
+      
+  //   });
+
+  // });
+
+  // const checkPairs = document.querySelector("score")
+// card.checkIfPair(card1, card2);
+//call .checkIfPair(card1, card2) method. 
+//If the two cards are the same, 
+//they should get class blocked, 
+//which is going to keep them flipped so we can see images.
+//});
+
+// memoryGame.addEventListener("click", () =>{
+  
+  // gameScreen.classList.block("true");
+  
+// });
+
+
+// let cardTurned = 0;
+// mouseClicked = function() {
+//     for (let i = 0; i < cards.length; i++) {
+//         let card = cards[i];
+
+
+
+memoryGame.cards.checkIfFinished((cards) =>{
+  return `You won!!`
+} 
+)
+
+// Hint 1: The array of picked cards can't ever hold more than two cards. 
+//Hint 2: Make sure you call checkIfFinished method 
+//to check if the condition for the end of the game is true, 
+//and if so, you can just alert the end, 
+//or be more creative and add some text on the canvas - displaying You won!!!
+
+
+// const startGame = () => {
+//   state.gameStarted = true
+//   selectors.start.classList.add('disabled')
+
+//   state.loop = setInterval(() => {
+//       state.totalTime++
+
+//       selectors.moves.innerText = `${state.totalFlips} moves`
+//       selectors.timer.innerText = `time: ${state.totalTime} sec`
+//   }, 1000)
+// }
--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,70 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
+
+
+  // Card(suit, rank) {
+  //   this.suit = suit;
+  //   this.rank = rank;
+  // }
+  
+  // Deck() {
+  //   this.cards = [];
+  //   for (suit = 4; suit > 0; suit--) {
+  //     for (rank = 13; rank > 0; rank--) {
+  //       this.cards.push(new Card(suit, rank));
+  //     }
+  //   }
+  // }
+  // let deck = new Deck();
 
   shuffleCards() {
-    // ... write your code here
+    let j, x, i;
+    if (!this.cards){
+      return undefined
+    }
+    for (i = this.cards.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = this.cards[i];
+        this.cards[i] = this.cards[j];
+        this.cards[j] = x;
+    }
+    
   }
+    // -- To shuffle an array a of n elements (indices 0..n-1):
+    // for i from n−1 down to 1 do
+    //      j ← random integer such that 0 ≤ j ≤ i
+    //      exchange a[j] and a[i]
+    // should return the shuffled (mixed) array of cards
+  
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+   // ex card1="ironman"
+   // ex card2="batman"
+   
+   this.pairsClicked += 1;
+   if (card1 === card2){
+    this.pairsGuessed += 1;
+
+    return true
+   } else {
+    return false
+   }
+   
   }
 
   checkIfFinished() {
-    // ... write your code here
+    // Here we need to check if our property 
+    //pairsGuessed has reached the numbers of pairs the game has.
+    if(this.pairsGuessed === this.cards.length/2){
+      return true
+    } else {
+      return false
+    }
+
   }
 }
